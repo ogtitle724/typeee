7dfaecd30d00a6293ff3cd719bc049f7f05b0373
ogtitle724 <wje0724@gmail.com>
2024-05-28 18:36:46 +0900

    ADJUST gtm

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import { getMetadata } from "@/config/metadata";
 import { headers } from "next/headers";
 import { Suspense } from "react";
 import Header from "@comps/header/header_cli/header";
-import Script from "next/script";
+import Analytics from "./_components/gtm/gtm";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,28 +25,7 @@ export default function RootLayout({ children }) {
           <NavigationEvents />
         </SessionWrapper>
         <Suspense>
-          <noscript>
-            <iframe
-              src={`https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM_ID}`}
-              height="0"
-              width="0"
-              style={{ display: "none", visibility: "hidden" }}
-            />
-          </noscript>
-          <Script
-            id="gtm-script"
-            nonce={nonce}
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer', "${process.env.NEXT_PUBLIC_GTM_ID}");
-  `,
-            }}
-          />
+          <Analytics nonce={nonce} />
         </Suspense>
         <footer></footer>
       </body>
